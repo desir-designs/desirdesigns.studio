096b5f6faab86657dedfe25cd39a7272c4c379ff
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-29 14:15:08 -0400

    Please

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@libs/tailwind.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function Application({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Application
