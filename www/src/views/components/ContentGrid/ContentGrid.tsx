d12e0e6cbe4769985f438bb50c9b6f9954a92421
Jeffrey Desir <jeffrey@desir.tech>
2023-07-11 19:10:33 -0400

    [][][]

--- www/src/views/components/ContentGrid/ContentGrid.tsx
+++ www/src/views/components/ContentGrid/ContentGrid.tsx
@@ -1,31 +1,150 @@
-export default function ContentGrid() {
+export const defaultProps = {
+  title: "Desir Designs",
+};
+
+export type ContentGridProps = {
+  title?: string | React.ReactNode;
+};
+
+export default function ContentGrid(props: ContentGridProps) {
+  
+  let { title } = props;
+
+
+  function Header() {
+    return (
+      <div className="mx-auto mb-20 text-center md:max-w-lg">
+      <h1 className="mb-4 font-semibold text-white text-9xl font-heading sm:text-7xl">
+        {title ?? defaultProps.title}
+      </h1>
+      <p className="text-lg text-gray-400">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim.
+      </p>
+    </div>
+    )
+  }
+
+  function Content() {
+    return (
+      <div className="flex flex-wrap -m-3">
+      <div className="w-full p-3 md:w-1/2 xl:w-1/4">
+        <a className="group" href="#">
+          <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
+            <img
+              className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
+              src="gradia-assets/images/blog/blog1.png"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 w-full p-2.5">
+              <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
+                  Technology . 4 min read
+                </h2>
+                <p className="text-lg font-semibold text-white font-heading group-hover:underline">
+                  You will never believe these bizarre truth of travel.
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div className="w-full p-3 md:w-1/2 xl:w-1/4">
+        <a className="group" href="#">
+          <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
+            <img
+              className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
+              src="gradia-assets/images/blog/blog2.png"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 w-full p-2.5">
+              <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
+                  Technology . 4 min read
+                </h2>
+                <p className="text-lg font-semibold text-white font-heading group-hover:underline">
+                  You will never believe these bizarre truth of travel.
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div className="w-full p-3 md:w-1/2 xl:w-1/4">
+        <a className="group" href="#">
+          <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
+            <img
+              className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
+              src="gradia-assets/images/blog/blog3.png"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 w-full p-2.5">
+              <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
+                  Technology . 4 min read
+                </h2>
+                <p className="text-lg font-semibold text-white font-heading group-hover:underline">
+                  You will never believe these bizarre truth of travel.
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div className="w-full p-3 md:w-1/2 xl:w-1/4">
+        <a className="group" href="#">
+          <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
+            <img
+              className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
+              src="gradia-assets/images/blog/blog4.png"
+              alt=""
+            />
+            <div className="absolute bottom-0 left-0 w-full p-2.5">
+              <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
+                  Technology . 4 min read
+                </h2>
+                <p className="text-lg font-semibold text-white font-heading group-hover:underline">
+                  You will never believe these bizarre truth of travel.
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    )
+  }
+
+  
+
   return (
-    <section className="pt-20 pb-36 bg-black overflow-hidden">
-      <div className="container mx-auto px-4">
-        <div className="md:max-w-lg mx-auto text-center mb-20">
-          <h2 className="mb-4 font-heading font-semibold text-white text-6xl sm:text-7xl">
-            Latest news
-          </h2>
+    <section className="h-screen pt-20 overflow-hidden bg-black bg-opacity-20 pb-36">
+      <div className="container px-4 mx-auto">
+        <div className="mx-auto mb-20 text-center md:max-w-lg">
+          <h1 className="mb-4 font-semibold text-white text-9xl font-heading ">
+            {title ?? defaultProps.title}
+          </h1>
           <p className="text-lg text-gray-400">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim.
           </p>
         </div>
         <div className="flex flex-wrap -m-3">
-          <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+          <div className="w-full p-3 md:w-1/2 xl:w-1/4">
             <a className="group" href="#">
-              <div className="group flex flex-col justify-end h-full relative overflow-hidden rounded-10">
+              <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
                 <img
-                  className="mx-auto w-full transform group-hover:scale-110 transition ease-out duration-500"
+                  className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
                   src="gradia-assets/images/blog/blog1.png"
                   alt=""
                 />
                 <div className="absolute bottom-0 left-0 w-full p-2.5">
-                  <div className="p-5 w-full bg-black bg-opacity-80 rounded-md">
-                    <h2 className="mb-2 font-heading font-medium text-xs uppercase text-gray-400 tracking-px">
+                  <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                    <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
                       Technology . 4 min read
                     </h2>
-                    <p className="font-heading font-semibold text-lg text-white group-hover:underline">
+                    <p className="text-lg font-semibold text-white font-heading group-hover:underline">
                       You will never believe these bizarre truth of travel.
                     </p>
                   </div>
@@ -33,20 +152,20 @@ export default function ContentGrid() {
               </div>
             </a>
           </div>
-          <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+          <div className="w-full p-3 md:w-1/2 xl:w-1/4">
             <a className="group" href="#">
-              <div className="group flex flex-col justify-end h-full relative overflow-hidden rounded-10">
+              <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
                 <img
-                  className="mx-auto w-full transform group-hover:scale-110 transition ease-out duration-500"
+                  className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
                   src="gradia-assets/images/blog/blog2.png"
                   alt=""
                 />
                 <div className="absolute bottom-0 left-0 w-full p-2.5">
-                  <div className="p-5 w-full bg-black bg-opacity-80 rounded-md">
-                    <h2 className="mb-2 font-heading font-medium text-xs uppercase text-gray-400 tracking-px">
+                  <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                    <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
                       Technology . 4 min read
                     </h2>
-                    <p className="font-heading font-semibold text-lg text-white group-hover:underline">
+                    <p className="text-lg font-semibold text-white font-heading group-hover:underline">
                       You will never believe these bizarre truth of travel.
                     </p>
                   </div>
@@ -54,20 +173,20 @@ export default function ContentGrid() {
               </div>
             </a>
           </div>
-          <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+          <div className="w-full p-3 md:w-1/2 xl:w-1/4">
             <a className="group" href="#">
-              <div className="group flex flex-col justify-end h-full relative overflow-hidden rounded-10">
+              <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
                 <img
-                  className="mx-auto w-full transform group-hover:scale-110 transition ease-out duration-500"
+                  className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
                   src="gradia-assets/images/blog/blog3.png"
                   alt=""
                 />
                 <div className="absolute bottom-0 left-0 w-full p-2.5">
-                  <div className="p-5 w-full bg-black bg-opacity-80 rounded-md">
-                    <h2 className="mb-2 font-heading font-medium text-xs uppercase text-gray-400 tracking-px">
+                  <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                    <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
                       Technology . 4 min read
                     </h2>
-                    <p className="font-heading font-semibold text-lg text-white group-hover:underline">
+                    <p className="text-lg font-semibold text-white font-heading group-hover:underline">
                       You will never believe these bizarre truth of travel.
                     </p>
                   </div>
@@ -75,20 +194,20 @@ export default function ContentGrid() {
               </div>
             </a>
           </div>
-          <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+          <div className="w-full p-3 md:w-1/2 xl:w-1/4">
             <a className="group" href="#">
-              <div className="group flex flex-col justify-end h-full relative overflow-hidden rounded-10">
+              <div className="relative flex flex-col justify-end h-full overflow-hidden group rounded-10">
                 <img
-                  className="mx-auto w-full transform group-hover:scale-110 transition ease-out duration-500"
+                  className="w-full mx-auto transition duration-500 ease-out transform group-hover:scale-110"
                   src="gradia-assets/images/blog/blog4.png"
                   alt=""
                 />
                 <div className="absolute bottom-0 left-0 w-full p-2.5">
-                  <div className="p-5 w-full bg-black bg-opacity-80 rounded-md">
-                    <h2 className="mb-2 font-heading font-medium text-xs uppercase text-gray-400 tracking-px">
+                  <div className="w-full p-5 bg-black rounded-md bg-opacity-80">
+                    <h2 className="mb-2 text-xs font-medium text-gray-400 uppercase font-heading tracking-px">
                       Technology . 4 min read
                     </h2>
-                    <p className="font-heading font-semibold text-lg text-white group-hover:underline">
+                    <p className="text-lg font-semibold text-white font-heading group-hover:underline">
                       You will never believe these bizarre truth of travel.
                     </p>
                   </div>
--- www/src/views/includes/NavBar/NavBar.tsx
+++ www/src/views/includes/NavBar/NavBar.tsx
@@ -1,90 +1,57 @@
 export default function NavBar() {
-    return (
-        <section className="overflow-hidden">
-  <div className="flex items-center justify-between px-8 py-5 bg-gray-900">
-    <div className="w-auto">
-      <div className="flex flex-wrap items-center">
-        <div className="w-auto mr-14">
-          <a href="#">
-            <img src="gradia-assets/logos/gradia-name-white.svg" alt="" />
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="hidden w-auto lg:block">
-      <ul className="flex items-center mr-10">
-        <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
-          <a href="#">Features</a>
-        </li>
-        <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
-          <a href="#">Solutions</a>
-        </li>
-        <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
-          <a href="#">Resources</a>
-        </li>
-        <li className="text-lg text-white font-heading hover:text-gray-200">
-          <a href="#">Pricing</a>
-        </li>
-      </ul>
-    </div>
-    <div className="w-auto">
-      <div className="flex flex-wrap items-center">
-        <div className="hidden w-auto lg:block">
-          <button className="group relative p-0.5 font-heading block w-full md:w-auto text-lg text-white font-medium bg-gradient-cyan overflow-hidden rounded-10">
-            <div className="absolute top-0 left-0 w-full h-full transition duration-500 ease-in-out transform -translate-y-full group-hover:-translate-y-0 bg-gradient-cyan" />
-            <div className="px-5 py-2 bg-gray-900 rounded-lg">
-              <p className="relative z-10">Start Free Trial</p>
-            </div>
-          </button>
-        </div>
-        <div className="w-auto lg:hidden">
-          <a href="#">
-            <svg
-              className="text-gray-800 navbar-burger"
-              width={51}
-              height={51}
-              viewBox="0 0 56 56"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width={56} height={56} rx={28} fill="currentColor" />
-              <path
-                d="M37 32H19M37 24H19"
-                stroke="white"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="fixed top-0 bottom-0 left-0 z-50 hidden w-4/6 navbar-menu sm:max-w-xs">
-    <div className="fixed inset-0 bg-gray-800 navbar-backdrop opacity-80" />
-    <nav className="relative z-10 h-full py-8 bg-gray-900 px-9">
-      <div className="flex flex-wrap justify-between h-full">
-        <div className="w-full">
-          <div className="flex items-center justify-between -m-2">
-            <div className="w-auto p-2">
-              <a className="inline-block" href="#">
+  return (
+    <section className="sticky top-0 overflow-hidden">
+      <div className="sticky flex justify-between px-8 py-5 top-0items-center backdrop-blur-sm">
+        <div className="w-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-auto mr-14">
+              <a href="#">
                 <img src="gradia-assets/logos/gradia-name-white.svg" alt="" />
               </a>
             </div>
-            <div className="w-auto p-2">
-              <a className="navbar-burger" href="#">
+          </div>
+        </div>
+        <div className="hidden w-auto lg:block">
+          <ul className="flex items-center mr-10">
+            <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
+              <a href="#">Features</a>
+            </li>
+            <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
+              <a href="#">Solutions</a>
+            </li>
+            <li className="text-lg text-white font-heading mr-9 hover:text-gray-200">
+              <a href="#">Resources</a>
+            </li>
+            <li className="text-lg text-white font-heading hover:text-gray-200">
+              <a href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="hidden w-auto lg:block">
+              <button className="group relative p-0.5 font-heading block w-full md:w-auto text-lg text-white font-medium bg-gradient-cyan overflow-hidden rounded-10">
+                <div className="absolute top-0 left-0 w-full h-full transition duration-500 ease-in-out transform -translate-y-full group-hover:-translate-y-0 bg-gradient-cyan" />
+                <div className="px-5 py-2 bg-gray-900 rounded-lg">
+                  <p className="relative z-10">Start Free Trial</p>
+                </div>
+              </button>
+            </div>
+            <div className="w-auto lg:hidden">
+              <a href="#">
                 <svg
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
+                  className="text-gray-800 navbar-burger"
+                  width={51}
+                  height={51}
+                  viewBox="0 0 56 56"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
+                  <rect width={56} height={56} rx={28} fill="currentColor" />
                   <path
-                    d="M6 18L18 6M6 6L18 18"
-                    stroke="#ffffff"
-                    strokeWidth={2}
+                    d="M37 32H19M37 24H19"
+                    stroke="white"
+                    strokeWidth="1.5"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
@@ -93,65 +60,100 @@ export default function NavBar() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-full py-8 text-center">
-          <ul>
-            <li className="mb-12">
-              <a
-                className="text-lg font-medium text-white font-heading hover:text-gray-200"
-                href="#"
-              >
-                Features
-              </a>
-            </li>
-            <li className="mb-12">
-              <a
-                className="text-lg font-medium text-white font-heading hover:text-gray-200"
-                href="#"
-              >
-                Solutions
-              </a>
-            </li>
-            <li className="mb-12">
-              <a
-                className="text-lg font-medium text-white font-heading hover:text-gray-200"
-                href="#"
-              >
-                Resources
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-lg font-medium text-white font-heading hover:text-gray-200"
-                href="#"
-              >
-                Pricing
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col justify-end w-full">
-          <div className="flex flex-wrap">
-            <div className="w-full mb-3">
-              <button className="p-0.5 font-heading block w-full text-lg text-white font-medium rounded-10">
-                <div className="px-5 py-2 rounded-10">
-                  <p>Login</p>
-                </div>
-              </button>
-            </div>
+      </div>
+      <div className="fixed top-0 bottom-0 left-0 z-50 hidden w-4/6 navbar-menu sm:max-w-xs">
+        <div className="fixed inset-0 bg-gray-800 navbar-backdrop opacity-80" />
+        <nav className="relative z-10 h-full py-8 bg-gray-900 px-9">
+          <div className="flex flex-wrap justify-between h-full">
             <div className="w-full">
-              <button className="group relative p-0.5 font-heading block w-full text-lg text-white font-medium bg-gradient-cyan overflow-hidden rounded-10">
-                <div className="absolute top-0 left-0 w-full h-full transition duration-500 ease-in-out transform -translate-y-full group-hover:-translate-y-0 bg-gradient-cyan" />
-                <div className="px-5 py-2 bg-gray-900 rounded-lg">
-                  <p className="relative z-10">Start Free Trial</p>
+              <div className="flex items-center justify-between -m-2">
+                <div className="w-auto p-2">
+                  <a className="inline-block" href="#">
+                    <img
+                      src="gradia-assets/logos/gradia-name-white.svg"
+                      alt=""
+                    />
+                  </a>
                 </div>
-              </button>
+                <div className="w-auto p-2">
+                  <a className="navbar-burger" href="#">
+                    <svg
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6 18L18 6M6 6L18 18"
+                        stroke="#ffffff"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center w-full py-8 text-center">
+              <ul>
+                <li className="mb-12">
+                  <a
+                    className="text-lg font-medium text-white font-heading hover:text-gray-200"
+                    href="#"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li className="mb-12">
+                  <a
+                    className="text-lg font-medium text-white font-heading hover:text-gray-200"
+                    href="#"
+                  >
+                    Solutions
+                  </a>
+                </li>
+                <li className="mb-12">
+                  <a
+                    className="text-lg font-medium text-white font-heading hover:text-gray-200"
+                    href="#"
+                  >
+                    Resources
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-lg font-medium text-white font-heading hover:text-gray-200"
+                    href="#"
+                  >
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col justify-end w-full">
+              <div className="flex flex-wrap">
+                <div className="w-full mb-3">
+                  <button className="p-0.5 font-heading block w-full text-lg text-white font-medium rounded-10">
+                    <div className="px-5 py-2 rounded-10">
+                      <p>Login</p>
+                    </div>
+                  </button>
+                </div>
+                <div className="w-full">
+                  <button className="group relative p-0.5 font-heading block w-full text-lg text-white font-medium bg-gradient-cyan overflow-hidden rounded-10">
+                    <div className="absolute top-0 left-0 w-full h-full transition duration-500 ease-in-out transform -translate-y-full group-hover:-translate-y-0 bg-gradient-cyan" />
+                    <div className="px-5 py-2 bg-gray-900 rounded-lg">
+                      <p className="relative z-10">Start Free Trial</p>
+                    </div>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </nav>
       </div>
-    </nav>
-  </div>
-</section>
- 
-    )
+    </section>
+  );
 }
