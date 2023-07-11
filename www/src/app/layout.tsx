bfe4356ee820d0ff9c0c36f5b957a6023b612c60
Jeffrey Desir <jeffrey@desir.tech>
2023-07-11 18:30:13 -0400

    123

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -1,19 +1,22 @@
 import "@libs/globals.css"
 
+import NavBar from "@includes/NavBar"
+import Footer from "@includes/Footer"
+
 export const metadata = {
   title: 'Desir Designs',
   description: 'The Fringe of Desing',
 }
 
-
-export default function RootLayout({
-  children,
-}: {
+export type NextLayoutProps = {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout({ children }: NextLayoutProps) {
   return (
     <html lang="en">
       <body >
+        <NavBar/>
         {children}
       </body>
     </html>
--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
 import type { JSXComponentType } from "blakprint/dist/typings"
 
 
 export default function HomePage(): JSXComponentType<any> {
   return (
-    <></>
+    <>
+      <main>
+
+      </main>
+    </>
   )
 }
